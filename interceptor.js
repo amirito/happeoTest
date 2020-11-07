697969f1f2b0fc4ef187239006ea132f3df5ba75
Amir Mousavi <amirito@Amirs-MacBook-Pro.local>
2020-11-07 18:48:16 +0330

    test

--- interceptor.js
+++ interceptor.js
@@ -1,15 +1,57 @@
-
+var axios = require("axios");
 let reqqueue = [];
-module.exports = batchInterceptor = (instance) => { instance.interceptors.request.use( async request => {
+const params = [];
+
+module.exports = batchInterceptor = (instance) => { 
+    instance.interceptors.request.use( request => {
+    // console.log("request", request)
+        reqqueue = request;
         // Add your code here
-
-        return new Promise(resolve => {
-            reqqueue.push({ request: request, resolver: resolve });
-            console.log("reqqueue", reqqueue)
-        });
         
-    }, 
+        request.url = request.host + request.url;
 
-    error => Promise.reject(error));
+        new Promise(resolve => {
+            reqLength += 1; 
+        })
+        new Promise(resolve => {
+            params.push(request.params.ids)
+            reqqueue.params.ids = params;
+            console.log("reqqueue", params);
+            console.log("reqqueue.length", params.length);
+            if (params.length === 3) {
+                axios.get(request.url , {params: {ids: params}})
+                    .then(function (response) {
+                        // handle success
+                        console.log('data: ', response.data);
+                    })
+                    .catch(function (error) {
+                        // handle error
+                        console.log(error);
+                    })
+                    .then(function () {
+                        // always executed
+                    });
+            } 
+
+            throw new axios.Cancel('Operation canceled by the user.')
+        });
+
+        // return reqqueue;
+
+    }, error => Promise.reject(error));
+
+    // instance.interceptors.response.use(response=>{
+    //     console.log("response", response);
+
+    //     return response;
+        
+    // },
+    //     error=>{
+    //     // console.log("error: ", error)
+    //       const fallbackValue = [
+    //         {userId: "Not authorized",id: "aerw15311sq",
+    //          title: "Please try     again",completed: false}];
+    //        return Promise.reject(fallbackValue);}
+    //     );
 }
 
--- test.js
+++ test.js
@@ -6,11 +6,20 @@ function runTest() {
     const batchUrl = "/file-batch-api";
 
     // Should return [{id:"fileid1"},{id:"fileid2"}] 
-    apiClient.get(batchUrl, {params: {ids: ["fileid1","fileid2"]}});
+    apiClient.get(batchUrl, {params: {ids: ["fileid1","fileid2"]}}).then(function (response) {
+        console.log('1: ', response.data);
+    }).catch(function (error) {
+        // handle error
+        console.log('error ========> ', error);
+    });
     // Should return [{id:"fileid2"}] 
-    apiClient.get(batchUrl, {params: {ids: ["fileid2"]}});
+    apiClient.get(batchUrl, {params: {ids: ["fileid2"]}}).then(function (response) {
+        console.log('2: ', response.data);
+    });
     // Should reject as the fileid3 is missing from the response
-    apiClient.get(batchUrl, {params: {ids: ["fileid3"]}}); 
+    apiClient.get(batchUrl, {params: {ids: ["fileid3"]}}).then(function (response) {
+        console.log('3: ', response.data);
+    }); 
 
 }
 
